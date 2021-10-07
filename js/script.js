b3b36e2d94cee67ffbefef9544c2d76850d76115
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-07 14:34:04 +0200

    complete

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log ("inizio js")
+
+const nomeUtente = prompt("Inserisci il tuo nome")
+const cognomeUtente = prompt("Inserisci il tuo cognome")
+const coloreUtente = prompt("Inserisci il tuo colore preferito")
+
+console.log ("Dati utente:", nomeUtente, cognomeUtente, coloreUtente)
+
+alert(`${nomeUtente}${cognomeUtente}${coloreUtente}21`)
